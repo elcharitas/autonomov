ee1d337a1b59b53a34a277b563fdfe36582b8931
elcharitas <jonathanirhodia@gmail.com>
2022-03-04 11:26:56 +0100

    feat: fix issues with connect wallet

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,25 +1,42 @@
-import React from "react";
-import {
-    Avatar,
-    Flex,
-    Icon,
-    IconButton,
-    Input,
-    InputGroup,
-    InputLeftElement,
-    useColorModeValue,
-} from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Avatar, useConst, useControllableState } from "@chakra-ui/react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
 
-const Session = () => {
+export default function Session() {
+    const providerOptions = {
+        binancechainwallet: {
+            package: true,
+        },
+    };
+
+    const web3Modal = new Web3Modal({
+        chainId: "80001",
+        theme: "dark",
+        cacheProvider: false,
+        disableInjectedProvider: false,
+        providerOptions,
+    });
+
+    const [account, setAccount] = useState("");
+
+    const [avatar, setAvatar] = useState("");
+
+    const [provider, setProvider] = useState(null);
+
+    useEffect(() => {
+        Promise.resolve(
+            (async () => {
+                const instance = await web3Modal.connect();
+                const provider = new ethers.providers.Web3Provider(instance);
+                const account = await provider.listAccounts()[0];
+                setProvider(provider);
+                setAccount(account);
+            })()
+        );
+    });
+
     return (
-        <Avatar
-            ml="4"
-            size="sm"
-            name="anubra266"
-            src="https://avatars.githubusercontent.com/u/30869823?v=4"
-            cursor="pointer"
-        />
+        <Avatar ml="4" size="sm" src={avatar} name={account} cursor="pointer" />
     );
-};
-
-export default Session;
+}
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 /** The Name of our amazinng dApp */
 export const appName = "Autonomov Theater";
+
+export const infuraId = "765d4237ce7e4d999f706854d5b66fdc";
