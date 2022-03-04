35b7d1c562af47aa01926139d4ddaa02072775c3
LordYurei <newjang789@gmail.com>
2022-03-04 04:25:57 +0100

    feat: add divider to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Divider, useColorModeValue } from "@chakra-ui/react";
 import { FaClipboardCheck, FaRss } from "react-icons/fa";
 import { AiFillGift } from "react-icons/ai";
 import { BsGearFill } from "react-icons/bs";
@@ -10,43 +10,45 @@ import NavItem from "./NavItem";
 import Logo from "./Logo";
 
 const Sidebar = (props) => {
-    return (
-        <Box
-            as="nav"
-            pos="fixed"
-            top="0"
-            left="0"
-            zIndex="sticky"
-            h="full"
-            pb="10"
-            overflowX="hidden"
-            overflowY="auto"
-            bg={useColorModeValue("blackAlpha.900", "gray.800")}
-            borderColor={useColorModeValue("blackAlpha.900", "gray.700")}
-            borderRightWidth="1px"
-            w="60"
-            {...props}
-        >
-            <Flex px="4" py="5" align="center" justify={"space-around"}>
-                <Logo width={"120px"} />
-            </Flex>
-            <Flex
-                direction="column"
-                as="nav"
-                fontSize="sm"
-                color="gray.600"
-                aria-label="Main Navigation"
-                borderTop={"ActiveBorder"}
-            >
-                <NavItem icon={MdHome}>Home</NavItem>
-                <NavItem icon={FaRss}>Trending</NavItem>
-                <NavItem icon={HiCollection}>Collections</NavItem>
-                <NavItem icon={FaClipboardCheck}>Live Streams</NavItem>
-                <NavItem icon={AiFillGift}>Changelog</NavItem>
-                <NavItem icon={BsGearFill}>Settings</NavItem>
-            </Flex>
-        </Box>
-    );
+  return (
+    <Box
+      as="nav"
+      pos="fixed"
+      top="0"
+      left="0"
+      zIndex="sticky"
+      h="full"
+      pb="10"
+      overflowX="hidden"
+      overflowY="auto"
+      bg={useColorModeValue("blackAlpha.900", "gray.800")}
+      borderColor={useColorModeValue("blackAlpha.900", "gray.700")}
+      borderRightWidth="1px"
+      w="60"
+      {...props}>
+      <Flex px="4" py="5" align="center" justify={"space-around"}>
+        <Logo width={"120px"} />
+      </Flex>
+      <Divider />
+      <Flex
+        direction="column"
+        as="nav"
+        fontSize="sm"
+        color="gray.600"
+        aria-label="Main Navigation"
+        borderTop={"ActiveBorder"}>
+        <NavItem icon={MdHome}>Home</NavItem>
+        <NavItem icon={HiCollection}>Explore</NavItem>
+        <NavItem icon={FaRss}>Trending</NavItem>
+        <Divider />
+        <NavItem icon={HiCollection}>Collections</NavItem>
+        <NavItem icon={FaClipboardCheck}>Live Streams</NavItem>
+        <Divider />
+        <NavItem icon={AiFillGift}>Changelog</NavItem>
+        <NavItem icon={BsGearFill}>Settings</NavItem>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Sidebar;
