eaa4e912ae6f4d508fb652b65e558154b33d5b3e
LordYurei <newjang789@gmail.com>
2022-03-04 04:19:43 +0100

    feat: add live stream page

---
+++ src/pages/Stream.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import Page from "../layouts/Page";
+
+const Stream = () => {
+    return (
+        <Page>
+            <Box>Live peer World</Box>
+        </Page>
+    );
+};
+
+export default Stream;
