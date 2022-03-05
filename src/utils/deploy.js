4b18e1509998b65fd9a025708d5f449ee701575b
LordYurei <newjang789@gmail.com>
2022-03-05 02:46:44 +0100

    feat: add method to deploy contracts

--- src/utils/deploy.js
+++ src/utils/deploy.js
@@ -2,10 +2,17 @@ import { ethers, upgrades } from "hardhat";
 
 async function deploy(contract, args) {
     const Contract = await ethers.getContractFactory(contract);
+    const { address } = await Contract.deploy(...args);
+
+    return address;
+}
+
+async function deployProxy(contract, args) {
+    const Contract = await ethers.getContractFactory(contract);
     const proxy = await upgrades.deployProxy(Contract, args);
     await proxy.deployed();
 
     return proxy.address;
 }
 
-export { deploy };
+export { deploy, deployProxy };
