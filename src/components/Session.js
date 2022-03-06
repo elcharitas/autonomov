9adc8f9675fe1a88133bf143a0301df2fe4b57a2
elcharitas <jonathanirhodia@gmail.com>
2022-03-06 19:34:38 +0100

    chore: remove login

--- src/components/Session.js
+++ src/components/Session.js
@@ -4,17 +4,5 @@ import Button from "./Button";
 import { connectWallet } from "../utils/connect";
 
 export default function Session() {
-    const [isLogged, { on }] = useBoolean(
-        localStorage.getItem("WEB3_CONNECT_CACHED_PROVIDER")?.length > 0
-    );
-
-    return isLogged ? (
-        <Button
-            ml="4"
-            text="Connect Wallet"
-            onClick={connectWallet.bind({}, on)}
-        />
-    ) : (
-        <Avatar ml="4" size="sm" cursor="pointer" />
-    );
+    return <Avatar ml="4" size="sm" cursor="pointer" />;
 }
