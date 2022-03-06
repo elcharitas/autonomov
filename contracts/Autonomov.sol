0e936346c0d0b912aa4c3613c03de7f94221a37e
elcharitas <jonathanirhodia@gmail.com>
2022-03-06 18:05:17 +0100

    feat: make autonomov contract better

--- contracts/Autonomov.sol
+++ contracts/Autonomov.sol
@@ -5,9 +5,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Autonomov is ERC721 {
-    uint256 maxFee;
+    // max fee that can be paid for video
+    uint256 private maxFee;
+    // token to use for paying
     ERC20 private payToken;
     address private autonomov;
+
+    uint256 tokenId = 0;
 
     struct TokenData {
         uint256 price;
@@ -16,6 +20,7 @@ contract Autonomov is ERC721 {
         mapping(address => bool) access;
     }
 
+    // token datas by id
     mapping(uint256 => TokenData) private tokenData;
 
     constructor(uint256 _maxFee, address _payToken)
@@ -30,21 +35,25 @@ contract Autonomov is ERC721 {
      * @dev mint  a new collectable
      */
     function mint(
-        uint256 _tokenId,
         string memory tokenUri,
         string memory trailerURI,
         uint256 price
     ) external {
-        require(!_exists(_tokenId), "Token exists already");
         require(price > 0, "Fee is less than allowed value");
         require(price < maxFee, "Fee is more than allowed value");
-        tokenData[_tokenId].price = price;
-        tokenData[_tokenId].uri = tokenUri;
-        tokenData[_tokenId].trail = trailerURI;
-        tokenData[_tokenId].access[msg.sender] = true;
-        _mint(msg.sender, _tokenId);
+        // increase the tokenId if in use
+        if (_exists(tokenId)) tokenId++;
+        tokenData[tokenId].price = price;
+        tokenData[tokenId].uri = tokenUri;
+        tokenData[tokenId].trail = trailerURI;
+        tokenData[tokenId].access[msg.sender] = true;
+        // mint new token to sender
+        _mint(msg.sender, tokenId);
     }
 
+    /**
+     * @dev change current price.  Can only be called by owner
+     */
     function setCurrentPrice(uint256 _tokenId, uint256 price) public {
         require(_exists(_tokenId), "Video is yet to be created");
         require(price > 0, "Fee is less than allowed value");
@@ -65,8 +74,6 @@ contract Autonomov is ERC721 {
         override
         returns (string memory)
     {
-        require(_exists(_tokenId), "Video is yet to be created");
-
         if (tokenData[_tokenId].access[msg.sender] == true) {
             return tokenData[_tokenId].uri;
         } else {
@@ -86,15 +93,30 @@ contract Autonomov is ERC721 {
     }
 
     /**
+     * @dev returns the token url for a collectable
+     */
+    function getList(uint256 page) external view returns (string[] memory) {
+        require(page > 0, "Page number should be more than zero");
+        string[] memory result;
+        uint256 perpage = 15;
+
+        for (uint256 i = 1; i < (perpage * page); i++) {
+            result[i] = tokenURI(i);
+        }
+
+        return result;
+    }
+
+    /**
      * @dev pay creators the view fee
      */
     function _payCreator(uint256 _tokenId) private {
         address owner = ownerOf(_tokenId);
-        require(msg.sender != owner, "You cant pay yourself");
+        require(msg.sender != owner, "You can't pay yourself");
         require(_exists(_tokenId), "Video is yet to be created");
         uint256 amount = tokenData[_tokenId].price;
 
-        (bool paid, ) = payable(owner).call{value: amount}("");
-        require(paid, "You need to pay owner to view video");
+        payToken.transferFrom(msg.sender, autonomov, (amount * 7) / 100);
+        payToken.transferFrom(msg.sender, owner, (amount * 93) / 100);
     }
 }
