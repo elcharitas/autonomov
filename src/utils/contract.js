682efa8f635a8a5d94ae8f977acea180839ef22f
elcharitas <jonathanirhodia@gmail.com>
2022-03-06 18:53:17 +0100

    chore: seee if

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -4,10 +4,12 @@ import { contract_abi, contract_addr } from "../constants";
 
 export async function getContract() {
     const provider = await getProvider();
+    await provider.send("eth_requestAccounts", []);
     return new ethers.Contract(contract_addr, contract_abi, provider);
 }
 
 export async function mintVideo(uri, trailer, price) {
     const contract = await getContract();
+    console.log(await contract.deployed());
     console.log(await contract.tokenURI(1));
 }
